20db9f99ab84e295eb835f2cf8beb4db83bb9b8c
HellHorseman <duke.sheffer@yandex.ru>
2023-04-13 20:04:05 +0400

    add students

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    Gryffindor[] gryffindors = new Gryffindor[]{
+            new Gryffindor("Гарри", "Поттер", 60, 45, 70, 62, 81),
+            new Gryffindor("Гермиона", "Грейнджер", 92, 64,65,70,56),
+            new Gryffindor("Рон", "Уизли",35,22,63,74,85)
+    };
+    Hufflepuff[] hufflepuffs = new Hufflepuff[]{
+            new Hufflepuff("Захария", "Смит",85,37,52,48,91),
+            new Hufflepuff("Седрик", "Диггори", 73,49,69,57,82),
+            new Hufflepuff("Джастин", "Финч-Флетчли", 66,58,64,52,61)
+    };
+    Ravenclaw[] ravenclaws = new Ravenclaw[]{
+            new Ravenclaw("Чжоу", "Чанг",73, 69,75,64,43,54),
+            new Ravenclaw("Падма", "Патил", 69,55,64,84,51,43),
+            new Ravenclaw("Маркус", "Белби", 49,67,54,23,81,23)
+    };
+    Slytherin[] slytherins = new Slytherin[]{
+            new Slytherin("Драко", "Малфой", 77, 64, 54,62,74,31,59),
+            new Slytherin("Грэхэм", "Монтегю", 37, 24, 47, 49, 56, 52,47),
+            new Slytherin("Грегори", "Гойл", 40,32,27,39,41,38,47)
+    };
 }
